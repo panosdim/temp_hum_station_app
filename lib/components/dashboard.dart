e06b95fb0c8c27dd6a25b972c582e44504eaec45
Panagiotis Dimopoulos <padi@intracom-telecom.com>
2020-04-22 13:22:07 +0300

    Rearrange Cards Layout and add new card.

--- lib/components/dashboard.dart
+++ lib/components/dashboard.dart
@@ -17,8 +17,18 @@ class DashboardWidget extends StatefulWidget {
 
 class _DashboardWidgetState extends State<DashboardWidget> {
   final f = new DateFormat('dd-MM-yyyy HH:00');
-  String _maxTempToday, _minTempToday, _maxTempMonth, _minTempMonth;
-  String _maxHumToday, _minHumToday, _maxHumMonth, _minHumMonth;
+  String _maxTempToday,
+      _minTempToday,
+      _maxTempMonth,
+      _minTempMonth,
+      _maxTempAllTime,
+      _minTempAllTime;
+  String _maxHumToday,
+      _minHumToday,
+      _maxHumMonth,
+      _minHumMonth,
+      _maxHumAllTime,
+      _minHumAllTime;
 
   @override
   void initState() {
@@ -84,6 +94,33 @@ class _DashboardWidgetState extends State<DashboardWidget> {
           .hum
           .toStringAsFixed(2);
     }
+
+    if (widget.sensorData.length == 0) {
+      _maxTempAllTime = "-";
+      _minTempAllTime = "-";
+      _maxHumAllTime = "-";
+      _minHumAllTime = "-";
+    } else {
+      _maxTempAllTime = widget.sensorData
+          .reduce((curr, next) => curr.temp > next.temp ? curr : next)
+          .temp
+          .toStringAsFixed(2);
+
+      _minTempAllTime = widget.sensorData
+          .reduce((curr, next) => curr.temp < next.temp ? curr : next)
+          .temp
+          .toStringAsFixed(2);
+
+      _maxHumAllTime = widget.sensorData
+          .reduce((curr, next) => curr.hum > next.hum ? curr : next)
+          .hum
+          .toStringAsFixed(2);
+
+      _minHumAllTime = widget.sensorData
+          .reduce((curr, next) => curr.hum < next.hum ? curr : next)
+          .hum
+          .toStringAsFixed(2);
+    }
   }
 
   bool isSameDate(DateTime that, DateTime other) {
@@ -110,6 +147,10 @@ class _DashboardWidgetState extends State<DashboardWidget> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
+                  Text(
+                    'Today',
+                    style: TextStyle(fontSize: 32),
+                  ),
                   Row(
                     children: <Widget>[
                       Icon(
@@ -120,10 +161,6 @@ class _DashboardWidgetState extends State<DashboardWidget> {
                       Expanded(
                         child: Column(
                           children: <Widget>[
-                            Text(
-                              'Today',
-                              style: TextStyle(fontSize: 32),
-                            ),
                             Text("Max " + _maxTempToday + " \u2103",
                                 style: TextStyle(fontWeight: FontWeight.bold)),
                             Text("Min " + _minTempToday + " \u2103",
@@ -131,16 +168,17 @@ class _DashboardWidgetState extends State<DashboardWidget> {
                           ],
                         ),
                       ),
+                      Icon(
+                        CustomIcons.humidity,
+                        size: 50,
+                        color: Colors.blue,
+                      ),
                       Expanded(
                         child: Column(
                           children: <Widget>[
-                            Text(
-                              'Month',
-                              style: TextStyle(fontSize: 32),
-                            ),
-                            Text("Max " + _maxTempMonth + " \u2103",
+                            Text("Max " + _maxHumToday + " \u0025",
                                 style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text("Min " + _minTempMonth + " \u2103",
+                            Text("Min " + _minHumToday + " \u0025",
                                 style: TextStyle(fontWeight: FontWeight.bold)),
                           ],
                         ),
@@ -160,8 +198,27 @@ class _DashboardWidgetState extends State<DashboardWidget> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
+                  Text(
+                    'Month',
+                    style: TextStyle(fontSize: 32),
+                  ),
                   Row(
                     children: <Widget>[
+                      Icon(
+                        CustomIcons.temperatire,
+                        size: 50,
+                        color: Colors.red,
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Text("Max " + _maxTempMonth + " \u2103",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text("Min " + _minTempMonth + " \u2103",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ),
                       Icon(
                         CustomIcons.humidity,
                         size: 50,
@@ -170,27 +227,60 @@ class _DashboardWidgetState extends State<DashboardWidget> {
                       Expanded(
                         child: Column(
                           children: <Widget>[
-                            Text(
-                              'Today',
-                              style: TextStyle(fontSize: 32),
-                            ),
-                            Text("Max " + _maxHumToday + " \u0025",
+                            Text("Max " + _maxHumMonth + " \u0025",
                                 style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text("Min " + _minHumToday + " \u0025",
+                            Text("Min " + _minHumMonth + " \u0025",
                                 style: TextStyle(fontWeight: FontWeight.bold)),
                           ],
                         ),
                       ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'All Time',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        CustomIcons.temperatire,
+                        size: 50,
+                        color: Colors.red,
+                      ),
                       Expanded(
                         child: Column(
                           children: <Widget>[
-                            Text(
-                              'Month',
-                              style: TextStyle(fontSize: 32),
-                            ),
-                            Text("Max " + _maxHumMonth + " \u0025",
+                            Text("Max " + _maxTempAllTime + " \u2103",
                                 style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text("Min " + _minHumMonth + " \u0025",
+                            Text("Min " + _minTempAllTime + " \u2103",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ),
+                      Icon(
+                        CustomIcons.humidity,
+                        size: 50,
+                        color: Colors.blue,
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Text("Max " + _maxHumAllTime + " \u0025",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text("Min " + _minHumAllTime + " \u0025",
                                 style: TextStyle(fontWeight: FontWeight.bold)),
                           ],
                         ),
